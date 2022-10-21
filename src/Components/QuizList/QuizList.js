9ec4fe77d42e48d6374b5d0d4da66fb792c0b883
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-10-21 22:11:04 +0600

    quizlist added

--- src/Components/QuizList/QuizList.js
+++ src/Components/QuizList/QuizList.js
@@ -9,19 +9,19 @@ const QuizList = ({qus,questions}) => {
   console.log(options)
   return (
     <div className="col">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{question}</h5>
-        {
-          options.map(option => <Options option={option} id={id} correctAnswer={correctAnswer} ></Options>)
-        }
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{question}</h5>
+          {
+            options.map(option => <Options option={option} id={id} correctAnswer={correctAnswer} ></Options>)
+          }
+        </div>
+        <div>
+          <EyeIcon className="text-blue-500 eyeion mb-3" onClick={() => setShow(!show)} />
+          {show ? <p className='text-success fw-bold'>Ans  :{correctAnswer}</p> : undefined}
+        </div>
+      </div>
     </div>
-    <div>
-        <EyeIcon className="text-blue-500 eyeion mb-3" onClick={() => setShow(!show)} />
-        {show ? <p className='text-success fw-bold'>Ans  :{correctAnswer}</p> : undefined}
-    </div>
-   </div>
-  </div>
   );
 };
 
