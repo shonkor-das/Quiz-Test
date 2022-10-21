37b8482bd2160bb24402340be6a13299922672ae
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-10-21 21:56:34 +0600

    Hader add in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,19 +6,15 @@ const Home = () => {
   let quizes = useLoaderData();
   return (
     <div className='container '>
-      
-    <div className='py-3'>
-    <h1 className="">Start the Quiz Test</h1>
-         <p className="">Good luck!</p>
-         <p className="card-text"><small></small></p>
+      <div className='py-3'>
+      <h1 className="">Start the Quiz Test</h1>
+      <p className="">Good luck!</p>
+      <p className="card-text"><small></small></p>
     </div>
       <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 m-3">
        {
-
           quizes.data.map(quiz => <Quizs quiz={quiz} key={quiz.id}></Quizs>)
-
        }
-     
       </div>
     </div>
   );
